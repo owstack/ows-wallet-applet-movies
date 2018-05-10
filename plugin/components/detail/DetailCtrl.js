f307b04c60f12dfce78a357f3d220472902721a2
ajp8164 <andyphillipson@gmail.com>
2018-05-10 09:29:45 -0400

    Update client dep, call chooseWallet from buy button, bump version 0.0.27

--- plugin/components/detail/DetailCtrl.js
+++ plugin/components/detail/DetailCtrl.js
@@ -1,9 +1,22 @@
 'use strict';
 
-angular.module('owsWalletPlugin.controllers').controller('DetailCtrl', function($scope, movieService) {
+angular.module('owsWalletPlugin.controllers').controller('DetailCtrl', function($scope, $log, movieService, CSession) {
 
   $scope.$on("$ionicView.beforeEnter", function(event, data) {
 	  $scope.movie = movieService.getMovie(data.stateParams.id);
   });
+
+  $scope.buy = function() {
+  	CSession.getInstance().chooseWallet().then(function(wallet) {
+  		if (!wallet) {
+	  		$log.debug('Use canceled');
+  		} else {
+	  		$log.debug('Wallet received');
+  		}
+
+  	}).catch(function(error) {
+  		$log.debug('Error choosing a wallet: ' + JSON.stringify(error));
+  	});
+  };
 
 });
